8dba582832f715023fa600155668e76a59e0d802
Jun <junbonifacio289@gmail.com>
2023-03-13 05:08:51 +0800

    Compendia

---
+++ Tcompendia/src/components/Features.vue
@@ -0,0 +1,108 @@
+<template>
+     <section class="bg-bookmark-purple py-20 mt-20 pb-60 lg:mt-60">
+        <!-- Heading-->
+        <div class="sm:w-3/4 lg:w-5/12 mx-auto px-2">
+            <h1 class="text-4xl text-center text-bookmark-white">Features</h1>
+            <p class="text-center text-bookmark-grey mt-4">This system will allow students to access the institute's library for academic research without having to go to the actual place.</p>
+        </div>
+        <!-- Feature #1-->
+        <div class="relative mt-20 lg:mt-24">
+            <div class="container flex flex-col lg:flex-row items-center justify-center gap-x-24">
+                <!-- Feature #1-->
+                <div class="flex flex-1 justify-center z-10 mb-10 lg:mb-0">
+                    <img src="../assets/images/Place.png" alt="" class="w-5/6 h-5/6 sm:w-3/4 sm:3/4 md:w-full md:h-full rounded-md">
+                </div>
+                <!-- Content-->
+                <div class="flex flex-1 flex-col items-center lg:items-start">
+                    <h1 class="text-3xl text-bookmark-white">Streamlining the research process</h1>
+                    <p class="text-bookmark-grey my-4 text-center lg:text-left sm:w-3/4 lg:w-full">With a web app research compendium, TIP students can easily access all the resources they need to conduct their research, including literature reviews, datasets, analysis tools, and other relevant information.</p>
+                    <p class="text-bookmark-grey my-4 text-center lg:text-left sm:w-3/4 lg:w-full">This can help to streamline the research process and save time, allowing students to focus more on conducting their research and less on finding resources.</p>
+                    <button type="button" class="btn btn-red hover:bg-bookmark-white hover:text-black">More Info</button>
+                </div>
+            </div>
+            <!-- Rounded rectangle -->
+            <div class="
+                    hidden 
+                    lg:block 
+                    overflow-hidden 
+                    bg-bookmark-dark 
+                    rounded-r-full 
+                    absolute 
+                    h-80 
+                    w-2/4 
+                    -bottom-24 
+                    -left-36
+                ">
+            </div>
+        </div>
+
+        <!-- Feature #2-->
+        <div class="relative mt-20 lg:mt-52">
+            <div class="container flex flex-col lg:flex-row-reverse items-center justify-center gap-x-24">
+                <!-- Feature #1-->
+                <div class="flex flex-1 justify-center z-10 mb-10 lg:mb-0">
+                    <img src="../assets/images/TIP1.png" alt="" class="w-5/6 h-5/6 sm:w-3/4 sm:3/4 md:w-full md:h-full rounded-md">
+                </div>
+                <!-- Content-->
+                <div class="flex flex-1 flex-col items-center lg:items-start">
+                    <h1 class="text-3xl text-bookmark-white">Enhancing research skills</h1>
+                    <p class="text-bookmark-grey my-4 text-center lg:text-left sm:w-3/4 lg:w-full"> A web app research compendium can also provide TIP students with the opportunity to enhance their research skills.</p>
+                    <p class="text-bookmark-grey my-4 text-center lg:text-left sm:w-3/4 lg:w-full">By providing access to a wide range of resources and tools, students can learn about new research techniques and methodologies, which can help them to become better researchers.</p>
+                    <button type="button" class="btn btn-red hover:bg-bookmark-white hover:text-black">More Info</button>
+                </div>
+            </div>
+            <!-- Rounded rectangle -->
+            <div class="
+                    hidden 
+                    lg:block 
+                    overflow-hidden 
+                    bg-bookmark-dark
+                    rounded-l-full 
+                    absolute 
+                    h-80 
+                    w-2/4 
+                    -bottom-24 
+                    -right-36
+                ">
+            </div>
+        </div>
+
+           <!-- Feature #3-->
+        <div class="relative mt-20 lg:mt-52">
+            <div class="container flex flex-col lg:flex-row items-center justify-center gap-x-24">
+                <!-- Feature #1-->
+                <div class="flex flex-1 justify-center z-10 mb-10 lg:mb-0">
+                    <img src="../assets/images/TIP2.png" alt="" class="w-5/6 h-5/6 sm:w-3/4 sm:3/4 md:w-full md:h-full rounded-md">
+                </div>
+                <!-- Content-->
+                <div class="flex flex-1 flex-col items-center lg:items-start">
+                    <h1 class="text-3xl text-bookmark-white">Encouraging collaboration</h1>
+                    <p class="text-bookmark-grey my-4 text-center lg:text-left sm:w-3/4 lg:w-full"> A web app research compendium can also encourage collaboration between TIP students.</p>
+                    <p class="text-bookmark-grey my-4 text-center lg:text-left sm:w-3/4 lg:w-full">By providing a central platform where students can share their research findings and collaborate on projects, students can learn from each other and work together to produce better research.</p>
+                    <button type="button" class="btn btn-red hover:bg-bookmark-white hover:text-black">More Info</button>
+                </div>
+            </div>
+            <!-- Rounded rectangle -->
+            <div class="
+                    hidden 
+                    lg:block 
+                    overflow-hidden 
+                    bg-bookmark-dark
+                    rounded-r-full 
+                    absolute 
+                    h-80 
+                    w-2/4 
+                    -bottom-24 
+                    -left-36
+                ">
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
